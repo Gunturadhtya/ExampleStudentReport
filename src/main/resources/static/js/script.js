ea86bdb92259a9804f1d723489db50fd190d9239
Lisa <fa037024@gmail.com>
2026-05-24 18:52:30 +0800

    refactor(ui): extract master data tab logic to external script

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -8,4 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
             fotoInput.click();
         })
     }
+
+    const triggerTabList = document.querySelectorAll('#masterDataTab button');
+
+    if (triggerTabList.length > 0) {
+        triggerTabList.forEach(triggerEl => {
+            triggerEl.addEventListener('click', event => {
+                triggerTabList.forEach(el => {
+                    el.classList.remove('border-bottom', 'border-3', 'border-primary', 'text-dark');
+                    el.classList.add('text-secondary');
+                });
+
+                event.target.classList.add('border-bottom', 'border-3', 'border-primary', 'text-dark');
+                event.target.classList.remove('text-secondary');
+            });
+        });
+    }
 });
